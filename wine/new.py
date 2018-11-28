ca4d771a03326b46902b183eb297074a60036eb4
ezana tesfaye <ezana.tesfaye@sjsu.edu>
2018-11-28 15:40:00 -0800

    edited files

---
+++ wine/new.py
@@ -0,0 +1,153 @@
+import csv
+import datetime
+import hashlib
+import math
+import os
+import pickle
+import numpy
+from textblob import TextBlob
+
+# wine id --> comments -> vector --ML--> wine id
+class wineClassifier:
+    # dict to put serialized file
+    storage = {}
+    wines = {}
+    distance_grid = {}
+    # input file
+    storage_file = "storage.pkl"  # location for the stored data
+    input_file = "test_part1.csv"  # raw data file
+
+    def normalize(self, score, low, high): #  possible function to scale sentiment
+        return  (float(score)-low)/(high-low)
+
+    def sigmoid(self, x): # possible function to scale sentiment
+      return 1 / (1 + math.exp(-x))
+
+    def __init__(self):
+        # load pickled file
+        if os.path.isfile(self.storage_file):
+            self.storage = pickle.load(open(self.storage_file, "rb"))
+            self.wines = self.storage["wines"]
+            self.distance_grid = self.storage["distance_grid"]
+
+
+        # check if the file is different
+        sha1 = hashlib.sha1()
+        with open(self.input_file, 'rb') as f:
+            while True:
+                data = f.read(65536)
+                if not data:
+                    break
+                sha1.update(data)
+        input_hash = sha1.hexdigest()
+
+        # input file is same as last time, stop init
+        if self.storage.get("hash") and input_hash == self.storage["hash"]:
+            print("same input file as last run")
+            return
+
+        # read from scrapped data into wines list
+        with open(self.input_file, "r" , encoding='utf-8') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in spamreader:
+                if row[0] == "wine_id":  # skip top row
+                    continue
+                wine_id = row[0]
+
+                if self.wines.get(wine_id):  # key exists in file, skip
+                    continue
+
+                avg_rating = float(row[1])
+                price = float(row[2])
+                variance = row[3]
+                vineyard = row[4]
+                region = row[5]
+                comments_detailed = row[6:]
+                comments = comments_detailed[2::3]
+                # print(comments)
+                comment_score = 0
+                comment_count = 0
+                # parse each comment
+                for comment in comments:
+                    # sentiment analysis
+                    test = TextBlob(comment)
+                    comment_score += test.sentiment.polarity
+                    comment_count += 1
+                avg_sentiment = comment_score / comment_count
+
+                self.wines[wine_id] = {}
+                self.wines[wine_id]["rating"] = avg_rating
+                self.wines[wine_id]["price"] = price
+                self.wines[wine_id]["sentiment"] = self.normalize(avg_sentiment, -1, 1)
+                self.wines[wine_id]["variance"] = variance
+                self.wines[wine_id]["vineyard"] = vineyard
+                self.wines[wine_id]["region"] = region
+
+            #  store evaluated data in file
+            self.generateDistanceGrid()
+            self.storage["wines"] = self.wines
+            self.storage["distance_grid"] = self.distance_grid
+            self.storage["hash"] = input_hash
+            pickle.dump(self.storage, open(self.storage_file, 'wb'))
+
+    """
+            this function generates a dict of dict that sorts all the wine based on its similarity to the current one
+             eg self.distance_grid[1][2] give the relative distance between wine 1 and wine 2
+    """
+    def generateDistanceGrid(self):
+        for wine_id, values in self.wines.items():
+            for test_id, test_values in self.wines.items():
+                if test_id == wine_id:  # skip the current one
+                    continue
+
+                if not self.distance_grid.get(wine_id):  # if new wine is loaded
+                    self.distance_grid[wine_id] = {}
+                if not self.distance_grid.get(test_id):  # fill in the opposite side of the matrix
+                    self.distance_grid[test_id] = {}
+                if self.distance_grid[wine_id].get(test_id): # skip if result exists
+                    continue
+
+                difference = self.getWineDifference(wine_id, test_id)
+                self.distance_grid[wine_id][test_id] = difference
+                self.distance_grid[test_id][wine_id] = difference
+
+    """
+      measure the difference between two wines by calcuating the norm, conditions can be changed to match under other 
+      conditions
+    """
+    def getWineDifference(self, a_id, b_id):
+        if a_id == b_id:
+            return 0
+        wine_a = self.getWineInfo(a_id)
+        wine_b = self.getWineInfo(b_id)
+        # find the difference in other factors
+        d_variance = 0
+        d_vineyard = 0
+        d_region = 0
+        if not wine_a["variance"] == wine_b["variance"]:
+            d_variance = 1
+        if not wine_a["region"] == wine_b["region"]:
+            d_region = 1
+        if not wine_a["vineyard"] == wine_b["vineyard"]:
+            d_vineyard = 1
+
+        a_array = numpy.array((wine_a["price"],wine_a["rating"],wine_a["sentiment"], 0, 0, 0))
+        b_array = numpy.array((wine_b["price"],wine_b["rating"],wine_b["sentiment"],d_variance,d_vineyard,d_region))
+        return numpy.linalg.norm(a_array - b_array)
+
+    def getClosestMatch(self, wineID):
+        return sorted(self.distance_grid[wineID], key=self.distance_grid[wineID].get)
+
+    def getWineInfo(self, wineID):
+        return self.wines.get(wineID)
+
+
+time = datetime.datetime.now()
+a = wineClassifier()
+diff = datetime.datetime.now() - time
+print("init time:", diff)
+time = datetime.datetime.now()
+print("Wine A:",a.getWineInfo("90577"))
+print("Wine that is similar:",a.getWineInfo(a.getClosestMatch("90577")[0]))
+diff = datetime.datetime.now() - time
+print("query time:", diff)
--- wine/routes.py
+++ wine/routes.py
@@ -1,10 +1,15 @@
-from flask import render_template,url_for
+from flask import render_template,url_for,request,flash
 from wine import app
 
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,PasswordField
 from wtforms.validators import DataRequired
 from wine.models import wine
+from wine.wineClass import wineClassifier
+
+
+
+a = wineClassifier()
 
 class wineForm(FlaskForm):
     name=StringField('Wine Name',validators=[DataRequired()])
@@ -25,10 +30,19 @@ class registerForm(FlaskForm):
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    form=wineForm()
-    if form.validate_on_submit():
-        wines=wine.query.all()
-        return render_template('rec.html',wines=wines)
+    form=wineForm(request.form)
+    if request.method == 'POST' and form.validate_on_submit():
+        # wines=wine.query.all()
+        user_input = form.name.data
+        wineInfo=a.getWineInfo(user_input)
+        # similar=a.getWineInfo(a.getClosestMatch(user_input)[0])
+        # print(similar[:10])
+        similar=a.getClosestMatch(user_input)[:10]
+        newList=[]
+        for id in similar:
+            info=a.getWineInfo(id)
+            newList.append(info)
+        return render_template('rec.html',newList=newList)
     return render_template('index.html',form=form)
 
 @app.route('/login',methods=['GET','POST'])
@@ -44,3 +58,5 @@ def register():
     if form.validate_on_submit():
         return render_template('rec.html')
     return render_template('account.html',form=form)
+# Wine A: {'rating': 3.7, 'price': 18.76, 'sentiment': 0.6183475748225075, 'variance': 'Cabernet Sauvignon', 'vineyard': 'Château de la Dauphine', 'region': 'Fronsac'}
+# Wine that is similar: {'rating': 3.7, 'price': 17.99, 'sentiment': 0.5325739483137195, 'variance': 'Cabernet Sauvignon', 'vineyard': 'Château Mayne-Vieil', 'region': 'Fronsac'}
