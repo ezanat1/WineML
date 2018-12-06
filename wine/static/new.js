ca118adac07dffcb71ba75a557ded2111efedc73
ezana tesfaye <ezana.tesfaye@sjsu.edu>
2018-12-05 17:31:53 -0800

    minor changes

--- wine/static/new.js
+++ wine/static/new.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
         else{
           $.each(data, function(key, value) {
           let mainDiv=$('<div class="col s3 m4"></div>')
-          let O=$('<div class="card "><div class="card-image waves-effect waves-block waves-light"><img src='+value.url+' style="max-height: 400px; padding: 10px"><a class="btn-floating right btn-small  waves-effect waves-light red addBtn" wineid='+value.id+'><i class="material-icons">add</i></a></div><div class="card-content"><span style="font-size:20px;font-family:cursive;overflow: hidden">'+value.name+ '</span><p><i class="material-icons">location_on</i>'+value.region+'</p><p><i class="material-icons">attach_money</i>'+value.price+'</p><p><i class="material-icons">restaurant_menu</i>'+value.pairing+'</p></div></div></div>')
+          let O=$('<div class="card "><div class="card-image waves-effect waves-block waves-light"><img src='+value.url+' style="max-height: 400px; padding: 10px"><a class="btn-floating right btn-small  waves-effect waves-light red addBtn" wineid='+value.id+'><i class="material-icons">add</i></a></div><div class="card-content"><span style="font-size:20px;overflow: hidden">'+value.name+ '</span><p><i class="material-icons">location_on</i>'+value.region+'</p><p><i class="material-icons">attach_money</i>'+value.price+'</p><p><i class="material-icons">restaurant_menu</i>'+value.pairing+'</p></div></div></div>')
           O.appendTo(mainDiv)
           mainDiv.appendTo('#result')
         });
